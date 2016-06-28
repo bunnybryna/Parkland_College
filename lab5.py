12fbfacd936f05a350ab9447a6caf3fd694030ab
bunnybryna <brynazhao@gmail.com>
2016-06-28 16:16:07 -0500

    lab5 assignment madlib
    Your program will have one main function named myStory() that takes 4 string arguments. Your program will print your story out using each argument that I have entered at the command line.Example of a command I will run and Output: 
    myStory("catfish","oranges","Rumplestilskin","ran up ") 
    My arguments in order will be:
    An Animal
    A Food
    A Person
    A Verb

---
+++ lab5.py
@@ -0,0 +1,10 @@
+def myStory(animal, food, person, verb):
+    line1 = "Once upon a time, there was a " + person + ","
+    line2 = " who was the bodyguard for a " + animal + "."
+    line3 = "The " + animal + " loves to eat Mcdonalds cheeseburger, pina coladas mixed with " + food + ","
+    line4 = "although it will feast on nearly anything sweet. "
+    line5 = "The " + person + " admires the " + animal
+    line6 = " because it can " + verb + " to unbelievable levels and rock his world."
+    print line1 + line2
+    print line3 + line4
+    print line5 + line6
